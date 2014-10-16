30ce745b37593684ad1ec833a018d0bc7334a062
Suhan Dharmasuriya <suhanr@wso2.com>
2014-10-16 10:34:41 +0530

    sample.db removed to fresh start

--- invoke.sh
+++ invoke.sh
@@ -1,14 +1,15 @@
+cd /var/www/www
 pip install flask
 pip --default-timeout=100 install flask-sqlalchemy
-pip install flask-bcrypt
-pip install flask-WTF
+pip --default-timeout=100 install flask-bcrypt
+pip --default-timeout=100 install flask-WTF
 pip install cherrypy
 export APP_SETTINGS="config.DevelopmentConfig"
 export PATH=$PATH:$APP_SETTINGS
 echo $APP_SETTINGS
 export DATABASE_URL="sqlite:////sample.db"
 echo $DATABASE_URL
-python db_create.py
-python db_create_users.py
-python sql.py
-python server.py
+python /var/www/www/db_create.py > /tmp/db_create.log
+python /var/www/www/db_create_users.py > /tmp/db_create_users.log
+python /var/www/www/sql.py > /tmp/sql.log
+python /var/www/www/server.py > /tmp/server.log
